e0d27e1e8b81f8bfdcac22ce18c16bbc2812299e
Jorge-Peralta <jorge9890@hotmail.com>
2022-06-14 23:25:00 -0600

    Fixed Cards Generation

--- Client/src/components/Home.js
+++ Client/src/components/Home.js
@@ -105,11 +105,11 @@ export default function Home() {
             </Stack>
           </Container>
         </Box>
-        <Container sx={{ py: 8 }} maxWidth="md">
+        <Container sx={{ py: 8 }} maxWidth="false">
           {/* End hero unit */}
-          <Grid container spacing={4}>
+          <Grid container spacing={8}>
             {list.map((lista) => (
-              <Grid item key={lista._id} xs={12} sm={6} md={4}>
+              <Grid item key={lista._id} xs={12} sm={6} md={4} lg={2}>
                 <Card
                   sx={{
                     height: "100%",
