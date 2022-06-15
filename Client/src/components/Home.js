9e21744156688873cec165d0a4cd078588145cb4
Jorge-Peralta <jorge9890@hotmail.com>
2022-06-14 22:09:58 -0600

    Fixed imges to fixed height
    Height of images

--- Client/src/components/Home.js
+++ Client/src/components/Home.js
@@ -1,34 +1,33 @@
-import * as React from 'react';
-import AppBar from '@mui/material/AppBar';
-import Button from '@mui/material/Button';
-import Card from '@mui/material/Card';
-import CardActions from '@mui/material/CardActions';
-import CardContent from '@mui/material/CardContent';
-import CardMedia from '@mui/material/CardMedia';
-import CssBaseline from '@mui/material/CssBaseline';
-import Grid from '@mui/material/Grid';
-import Stack from '@mui/material/Stack';
-import Box from '@mui/material/Box';
-import Toolbar from '@mui/material/Toolbar';
-import Typography from '@mui/material/Typography';
-import Container from '@mui/material/Container';
-import Link from '@mui/material/Link';
-import { createTheme, ThemeProvider } from '@mui/material/styles';
-import axios from 'axios';
-import {useState, useEffect} from 'react';
+import * as React from "react";
+import AppBar from "@mui/material/AppBar";
+import Button from "@mui/material/Button";
+import Card from "@mui/material/Card";
+import CardActions from "@mui/material/CardActions";
+import CardContent from "@mui/material/CardContent";
+import CardMedia from "@mui/material/CardMedia";
+import CssBaseline from "@mui/material/CssBaseline";
+import Grid from "@mui/material/Grid";
+import Stack from "@mui/material/Stack";
+import Box from "@mui/material/Box";
+import Toolbar from "@mui/material/Toolbar";
+import Typography from "@mui/material/Typography";
+import Container from "@mui/material/Container";
+import Link from "@mui/material/Link";
+import { createTheme, ThemeProvider } from "@mui/material/styles";
+import axios from "axios";
+import { useState, useEffect } from "react";
 import Rating from "@mui/material/Rating";
-import { useSelector } from 'react-redux';
-
+import { useSelector } from "react-redux";
 
 function Copyright() {
   return (
     <Typography variant="body2" color="text.secondary" align="center">
-      {'Copyright © '}
+      {"Copyright © "}
       <Link color="inherit" href="https://mui.com/">
         Cinema Project
-      </Link>{' '}
+      </Link>{" "}
       {new Date().getFullYear()}
-      {'.'}
+      {"."}
     </Typography>
   );
 }
@@ -36,19 +35,22 @@ function Copyright() {
 const theme = createTheme();
 
 export default function Home() {
-  const auth = useSelector(state => state.auth);
+  const auth = useSelector((state) => state.auth);
   const [list, setList] = useState([]);
 
-  useEffect( () => {
-    axios.get('http://localhost:3000/api/peliculas').then( res =>{
-      setList(res.data);
-    }).catch( error => {
-      console.log(error)
-    })
-  },[setList])
+  useEffect(() => {
+    axios
+      .get("http://localhost:3000/api/peliculas")
+      .then((res) => {
+        setList(res.data);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  }, [setList]);
 
   console.log(auth);
-  
+
   return (
     <ThemeProvider theme={theme}>
       <CssBaseline />
@@ -58,7 +60,7 @@ export default function Home() {
             Cinema Project
           </Typography>
           <Typography variant="h6" color="inherit" noWrap>
-             {auth.username}  
+            {auth.username}
           </Typography>
         </Toolbar>
       </AppBar>
@@ -66,7 +68,7 @@ export default function Home() {
         {/* Hero unit */}
         <Box
           sx={{
-            bgcolor: 'background.paper',
+            bgcolor: "background.paper",
             pt: 8,
             pb: 6,
           }}
@@ -81,8 +83,16 @@ export default function Home() {
             >
               Encuentra tus peliculas preferidas
             </Typography>
-            <Typography variant="h5" align="center" color="text.secondary" paragraph>
-              En Cinema Project te ayudamos a decidir que pelicula veras hoy, encuentra las mejores peliculas y forma parte de la comunidad al hacer tu propio review y ayudar a los demas a decidir que pelicula ver. 
+            <Typography
+              variant="h5"
+              align="center"
+              color="text.secondary"
+              paragraph
+            >
+              En Cinema Project te ayudamos a decidir que pelicula veras hoy,
+              encuentra las mejores peliculas y forma parte de la comunidad al
+              hacer tu propio review y ayudar a los demas a decidir que pelicula
+              ver.
             </Typography>
             <Stack
               sx={{ pt: 4 }}
@@ -101,18 +111,23 @@ export default function Home() {
             {list.map((lista) => (
               <Grid item key={lista._id} xs={12} sm={6} md={4}>
                 <Card
-                  sx={{ height: '100%', display: 'flex', flexDirection: 'column' }}
+                  sx={{
+                    height: "100%",
+                    display: "flex",
+                    flexDirection: "column",
+                  }}
                 >
                   <CardMedia
                     component="img"
                     sx={{
                       // 16:9
-                      pt: '56.25%',
+                      height: 550,
+                      pt: "56.25%",
                     }}
                     image={lista.imagen}
                     alt="pelicula"
                   />
-                   <CardContent sx={{ flexGrow: 1 }}>
+                  <CardContent sx={{ flexGrow: 1 }}>
                     <Typography gutterBottom variant="h4" component="h1">
                       {lista.filme}
                     </Typography>
@@ -122,14 +137,21 @@ export default function Home() {
                     <Typography gutterBottom variant="h6" component="h3">
                       Reparto: {lista.reparto}
                     </Typography>
-                    <Typography>
-                      {lista.sinopsis}
-                    </Typography>                
+                    <Typography>{lista.sinopsis}</Typography>
                   </CardContent>
-                  <Stack spacing={1} alignItems="left"
-  justifyContent="left" sx={{ m: 2}}>
-                      <Rating name="half-rating" defaultValue={parseInt(lista.rate)} precision={0.5} readOnly />
-                    </Stack>
+                  <Stack
+                    spacing={1}
+                    alignItems="left"
+                    justifyContent="left"
+                    sx={{ m: 2 }}
+                  >
+                    <Rating
+                      name="half-rating"
+                      defaultValue={parseInt(lista.rate)}
+                      precision={0.5}
+                      readOnly
+                    />
+                  </Stack>
                   <CardActions>
                     <Button size="small">Mirar Trailer</Button>
                     <Button size="small">Comentar</Button>
@@ -141,16 +163,14 @@ export default function Home() {
         </Container>
       </main>
       {/* Footer */}
-      <Box sx={{ bgcolor: 'background.paper', p: 6 }} component="footer">
-        <Typography variant="h6" align="center" gutterBottom>
-        </Typography>
+      <Box sx={{ bgcolor: "background.paper", p: 6 }} component="footer">
+        <Typography variant="h6" align="center" gutterBottom></Typography>
         <Typography
           variant="subtitle1"
           align="center"
           color="text.secondary"
           component="p"
-        >
-        </Typography>
+        ></Typography>
         <Copyright />
       </Box>
       {/* End footer */}
